e64bedf9197a5cccc9f9369828b008c711739a87
Yu Ishihara <ishihara.yu@gmail.com>
2018-12-28 16:45:54 +0900

    Add files

---
+++ Assets/TogglePostProcessing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class TogglePostProcessing : MonoBehaviour
+{
+    public PostProcessVolume volumeA;
+    public PostProcessVolume volumeB;
+
+    void TogglePostProcess()
+    {
+        if (volumeA.enabled)
+        {
+            volumeA.enabled = false;
+            volumeB.enabled = true;
+        }
+        else
+        {
+            volumeA.enabled = true;
+            volumeB.enabled = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 30), "TogglePostProcess"))
+        {
+            TogglePostProcess();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+### PostProcessingを切り替える方法
+
+![](images/0.png)
